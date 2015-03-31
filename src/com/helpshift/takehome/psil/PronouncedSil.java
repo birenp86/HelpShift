a112735dc5e99ae925e4a0bd02e15f15da006e2b
Biren Patel <birenp@yahoo-inc.com>
2015-03-31 14:00:40 -0700

    Adding USer Comments

--- src/com/helpshift/takehome/psil/PronouncedSil.java
+++ src/com/helpshift/takehome/psil/PronouncedSil.java
@@ -7,11 +7,14 @@ import java.util.Scanner;
 public class PronouncedSil {
 
 	private static Scanner stdin;
-
+	//main function
 	public static void main(String[] args) throws IOException {
+		//PsilCompute Object:
 		PsilCompute psilCompute = PsilCompute.getPage();
+		//Scanner Object for User MultiLine Input
 		stdin = new Scanner(new BufferedInputStream(System.in));
 		System.out.println("Enter Input -- \n\nTo Exit Type \"exit\"\n");
+		//Read User Input
 		psilCompute.readInput(stdin);
 	}
 }
